e80218413e7fe71f59b302c76f5a20d17db86474
Michal Malý <madcatxster@prifuk.cz>
2013-07-26 10:41:25 +0200

    Add version ID and generate version string.

--- globals.h
+++ globals.h
@@ -1,6 +1,11 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
+#define APP_NAME "FFBChecker"
+static const int APP_VERSION_MAJOR(0);
+static const int APP_VERSION_MINOR(1);
+static const char APP_VERSION_REL('a');
+
 enum class FFBEffectTypes { NONE, CONSTANT, PERIODIC, RAMP, SPRING, FRICTION, DAMPER, RUMBLE, INERTIA };
 enum class PeriodicWaveforms { NONE, SQUARE, TRIANGLE, SINE, SAW_UP, SAW_DOWN };
 
